8fe70b0d9c2608170eea285a34b90e2ee1751286
alexbanilean <alexandru.banilean@gmail.com>
2022-03-10 01:30:10 +0200

    node + ejs

---
+++ .history/index_20220310010454.js
@@ -0,0 +1,27 @@
+const express= require("express");
+const { redirect } = require("express/lib/response");
+app= express();
+
+app.set("view engine", "ejs");
+
+app.use("/resurse", express.static(__dirname+"/resurse"))
+
+app.get(["/", "/index", "/home"], function(req, res){
+    console.log(__dirname);
+    res.render("pagini/index");
+})
+
+app.get("/despre", function(req, res){
+    console.log(__dirname);
+    res.render("pagini/despre");
+})
+
+app.get("/*", function(req, res){
+    res.write("Cerere generala");
+    console.log("Cerere generala:", req.url);
+
+    res.end();
+})
+
+app.listen(8080);
+console.log("Am pornit!");
---
+++ .history/index_20220310010814.js
@@ -0,0 +1,28 @@
+const express= require("express");
+const { redirect } = require("express/lib/response");
+app= express();
+
+app.set("view engine", "ejs");
+
+app.use("/resurse", express.static(__dirname+"/resurse"))
+
+app.get(["/", "/index", "/home"], function(req, res){
+    console.log(__dirname);
+    res.render("pagini/index");
+})
+
+// app.get("/despre", function(req, res){
+//     console.log(__dirname);
+//     res.render("pagini/despre");
+// })
+
+app.get("/*", function(req, res){
+    res.render("pagini"+req.url);
+    function(err, rez)
+    console.log("Cerere generala:", req.url);
+
+    res.end();
+})
+
+app.listen(8080);
+console.log("Am pornit!");
